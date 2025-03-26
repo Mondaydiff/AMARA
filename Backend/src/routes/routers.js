16af2c192448058f9510b43e1c76dfcd1163f2fc
oweenunda <oweenunda.dev@gmail.com>
2025-03-26 06:07:55 -0500

    feature: api para compras y materias primas

--- Backend/src/routes/routers.js
+++ Backend/src/routes/routers.js
@@ -4,6 +4,7 @@ import authRoutes from './auth.routes.js'
 import { authMiddleware } from '../middlewares/auth.middleware.js'
 import createClientProveedor from './createCliente_proveedor.router.js'
 import materiaPrimaRouters from './materiaPrima.routers.js'
+import compraRouters from './compra.routers.js'
 const router = express.Router()
 
 router.use('/personas', personaRouters)
@@ -14,5 +15,6 @@ router.get('/perfil', authMiddleware, (req, res) => {
 router.use('/cliente', createClientProveedor)
 router.use('/proveedor', createClientProveedor)
 router.use('/materias-primas', materiaPrimaRouters)
+router.use('/compra', compraRouters)
 
 export default router
